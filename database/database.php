ced47a2c2c6926ed7e02d5a6296b6422a7a8aab0
Matteo <metboolean@gmail.com>
2021-03-02 17:44:55 +0100

    alf

---
+++ database/database.php
@@ -0,0 +1,73 @@
+<?php
+$dischi = [
+  [
+    'image' => "http://www.rapburger.com/wp-content/uploads/2013/09/BUo0lE0CYAMT7bP.jpg",
+    'titolo' => "The Marshall Mathers LP 2",
+    'autore' => "Eminem",
+    'genere' => "Rap",
+    'anno' => "2013"
+  ],
+  [
+    'image' => "https://upload.wikimedia.org/wikipedia/en/1/16/Alleyezonme.jpg",
+    'titolo' => "All Eyez on Me",
+    'autore' => "Tupac Shakur",
+    'genere' => "G-funk",
+    'anno' => "1996"
+  ],
+  [
+    'image' => "https://f4.bcbits.com/img/a3448488036_10.jpg",
+    'titolo' => "Live in the Street",
+    'autore' => "Dub FX",
+    'genere' => "Elettronica",
+    'anno' => "2007"
+  ],
+  [
+    'image' => "https://img.discogs.com/nh2evkXqbX80LIr25mhpKlQd4e0=/fit-in/590x449/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-3372949-1327834137.jpeg.jpg",
+    'titolo' => "Schöne neue Welt",
+    'autore' => "Culcha Candela",
+    'genere' => "Dancehall",
+    'anno' => "2009"
+  ],
+  [
+    'image' => "https://upload.wikimedia.org/wikipedia/en/e/e3/Audio_Bullys_-_Ego_War_album_cover.png",
+    'titolo' => "Ego War",
+    'autore' => "Audio Bullys",
+    'genere' => "Elettronica",
+    'anno' => "2003"
+  ],
+  [
+    'image' => "https://img.discogs.com/eXCHEfkkoK7wJH25UlRPrZfwgLY=/fit-in/577x573/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-1321885-1223502357.jpeg.jpg",
+    'titolo' => "Jump",
+    'autore' => "Kris Kross",
+    'genere' => "Hip-Hop",
+    'anno' => "1992"
+  ],
+  [
+    'image' => "http://2.bp.blogspot.com/-yfu3NY-6YEg/TukZo9KVx3I/AAAAAAAAIUQ/VS4r1z02oTk/s1600/avicii-levels-single-cover.jpg",
+    'titolo' => "Levels",
+    'autore' => "Avicii",
+    'genere' => "Progressive House",
+    'anno' => "2011"
+  ],
+  [
+    'image' => "https://media1.jpc.de/image/w600/front/0/0634904012120.jpg",
+    'titolo' => "The Fat of the Land",
+    'autore' => "The Prodigy",
+    'genere' => "Techno",
+    'anno' => "1997"
+  ],
+  [
+    'image' => "https://imagescdn.junodownload.com/full/CS4444123-02A-BIG.jpg",
+    'titolo' => "Against The Rest",
+    'autore' => "Twisted Harmonies",
+    'genere' => "Elettronica",
+    'anno' => "2019"
+  ],
+  [
+    'image' => "https://www.musicstore.sm/image/cache/catalog/musicstore/prodotti/060/0600753016121-800x800.jpg",
+    'titolo' => "Dire Straits",
+    'autore' => "Dire Straits",
+    'genere' => "Rock",
+    'anno' => "1978"
+  ],
+];
--- index.php
+++ index.php
@@ -3,11 +3,47 @@
   <head>
     <meta charset="utf-8">
     <title>Php Dischi</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css"
+        integrity="sha512-+4zCK9k+qNFUR5X+cKL9EIR+ZOhtIloNl9GIKS57V1MyNsYpYcUrUeQc9vNfzsWfV28IaLL3i96P9sdNyeRssA=="
+        crossorigin="anonymous" />
     <link rel="stylesheet" href="css/style.css">
   </head>
   <body>
+    <?php
+      require_once __DIR__ . '/database/database.php';
+    ?>
     <div class="container">
-      <?php echo 'Ciao'; ?>
+      <div class="header">
+        <div class="header-input">
+          <div>
+            <input type="text" name="" value="">
+          </div>
+          <div>
+            <i class="fas fa-search"></i>
+          </div>
+        </div>
+      </div>
+      <div class="main">
+
+        <?php
+        foreach($dischi as $disco) {
+        ?>
+
+        <div class="main-scheda">
+          <img src="<?php echo $dischi['image']; ?>">
+          <div class="main-scheda-text">
+            <div class="main-scheda-text-album"><?php echo $disco['titolo']; ?></div>
+            <div class="main-scheda-text-artista"><?php echo $disco['autore']; ?></div>
+            <div class="main-scheda-text-genere"><?php echo $disco['genere']; ?></div>
+            <div class="main-scheda-text-anno"><?php echo $disco['anno']; ?></div>
+          </div>
+        </div>
+
+        <?php
+          }
+        ?>
+
+      </div>
     </div>
   </body>
 </html>
