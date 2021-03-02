673869b4ca3e503f348f4445926448423dc764ec
Matteo <metboolean@gmail.com>
2021-03-02 15:44:26 +0100

    preparazione

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="it" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Php Dischi</title>
+    <link rel="stylesheet" href="css/style.css">
+  </head>
+  <body>
+    <div class="container">
+      <?php echo 'Ciao'; ?>
+    </div>
+  </body>
+</html>
